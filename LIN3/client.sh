d0d4c6328ee22cd9cf93d549fe8ae3eaa08186bf
Slendy_Milky <thegaming2001@gmail.com>
2021-12-07 23:54:10 +0100

    Update client.sh

--- LIN3/client.sh
+++ LIN3/client.sh
@@ -24,8 +24,10 @@ if [ $1 == "install" ]; then
 
     # Demande l'ip / url du serveur de stratum supérieur
     echo ""
-    read -n 15 -p "Écrivez l'ip du serveur ntp que vous voulez utilisez : " $1
+    read -n 15 -p "Écrivez l'ip du serveur ntp que vous voulez utilisez : " 1
     echo ""
+
+
 
     # Modifiez changme par $1
     sed -i 's/changme/$1/' /etc/ntp.conf
