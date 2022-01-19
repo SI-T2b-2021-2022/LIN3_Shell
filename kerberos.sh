693e4a35e6e44559935927d519c76ea8c77753d8
Slendy_Milky <thegaming2001@gmail.com>
2022-01-19 11:18:58 +0100

    Update kerberos.sh

--- kerberos.sh
+++ kerberos.sh
@@ -18,3 +18,45 @@ if [[ $(id -u) -ne 0 ]]; then
     echo ""
     exit 0
 fi
+
+# Check si argument donné
+if [[ -z $1 || -z $2 ]]; then
+    echo ""
+    echo "Argument invalide ! ❌"
+    echo "Usage   : $0 <client/server> <install/check>"
+    echo "Exemple : $0 client install"
+    echo ""
+    exit 0
+else
+    # Check if argument $1 est bon
+    if [ $1 != "client" ] && [ $1 != "server" ]; then
+        echo ""
+        echo "Argument '$1' invalide ! ❌"
+        echo "Usage   : $0 <client/server> <install/check>"
+        echo "Exemple : $0 client install"
+        echo ""
+        exit 0
+    fi
+
+    # Check if argument $2 est bon
+    if [ $2 != "check" ] && [ $2 != "install" ]; then
+        echo ""
+        echo "Argument '$2' invalide ! ❌"
+        echo "Usage   : $0 $1 <install/check>"
+        echo "Exemple : $0 $1 install"
+        echo ""
+        exit 0
+    fi
+
+    # Check si argument pour serveur kerberos personnalisé présent
+    if [[ $1 == "client" && $2 == "install"  ]]; then
+        if [ -z $3 ]; then
+            echo ""
+            echo "Serveur keberos manquant ! ❌"
+            echo "Usage   : $0 $1 $2"
+            echo "Exemple : $0 client install 10.0.1.11"
+            echo ""
+            exit 0
+        fi
+    fi
+fi
